f3cedcdc58c51c6710c0f6e86e4c12f3a33ba804
Terence Rustia <68271470+tsamba120@users.noreply.github.com>
2022-12-03 14:49:26 -0500

    implemented custom loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// network address
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// set up logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// Use the http.NewServeMux() function to initialize a new ServeMux (aka router)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -14,11 +24,17 @@ func main() {
 
 	// create file server that serves files out of "./ui/static/ directory"
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// instantiate a new http.Server struct but attached out custom error logger
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  mux,
+		ErrorLog: errorLog,
+	}
+
 	// Use http.ListenAndServe() to state a new webserver. Pass in TCP network address to listen on and servemux
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
